c0ef0bee8e91388efe1923fbfe4d80ac5c35db73
James Rogers <j_rogers@outlook.com>
2016-10-12 14:58:20 -0700

    refactoring

---
+++ util/get-links-from-site.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const htmlparser = require('htmlparser2');
+const https = require('https');
+
+let link = {},
+    links = [],
+    linkText = '',
+    storyLink = false;
+
+const parser = new htmlparser.Parser({
+    onopentag: ( name, attribs )=> {
+        if( name === 'tr' && attribs.class === 'athing' ){
+            link.id = parseInt( attribs.id );
+            storyLink = false;
+        } else if( name === 'a' && attribs.class === 'storylink' ){
+            storyLink =  true;
+            link.href = attribs.href;
+        } else {
+            storyLink = false;
+        }
+    },
+    ontext : ( text )=> {
+        if( storyLink ){
+            linkText += text;
+        }
+    },
+    onclosetag: ()=> {
+        if( storyLink ){
+            storyLink = false;
+            link.text = decodeURIComponent( linkText );
+            links.push( link );
+            link = {};
+            linkText = '';
+        }
+    }
+});
+
+const returnMatchingLinks = ( headlines )=> {
+    let matches = { "how": [], "what": [], "why": [] },
+        regex,
+        regexes = {
+            // only match 'how' when it's at the beginning of the string or part
+            // of 'here's how' which can appear anywhere.
+            how: /^(h|H)ow|((H|h)ere's) (h|H)ow/,
+            what: /\b(W|w)hat\b/,
+            why: /\b(W|w)hy\b/
+        };
+
+    for( linkText of headlines ) {
+        for( regex in regexes ) {
+            if( regexes[ regex ].test( linkText.text ) ){
+                matches[ regex ].push( linkText );
+            }
+        }
+    }
+
+    return matches;
+};
+
+exports.getLinks = () => {
+    return new Promise(
+        ( resolve, reject ) => {
+            https.get( 'https://news.ycombinator.com', ( res )=> {
+                res.on( 'data', ( d )=> {
+                    parser.write( d );
+                });
+            }).on( 'close', ()=> {
+                resolve( returnMatchingLinks( links ) );
+            });
+        }
+    );
+};
---
+++ util/write-links-to-database.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const pg = require('pg');
+// const pgp = require('pg-promise')({ ssl: process.env.NODE_ENV !== undefined });
+const linkLoader = require('./get-links-from-site');
+
+if( process.env.NODE_ENV !== undefined ){
+	pg.defaults.ssl = true;
+}
+
+let db = new pg.Client( process.env.DATABASE_URL );
+db.connect();
+
+let insertLinkIntoDatabase = ( link ) => {
+	db.query('INSERT INTO links(id, href, text) VALUES($1, $2, $3)', [link.id, link.href, link.text])
+		.catch((error)=> {
+			//meh
+		});
+};
+
+let writeSummaryToDatabase = ( ids )=> {
+	return db.query(
+		`INSERT INTO howwhatwhy(hows, whats, whys) VALUES('{${ids.how.join( ',' )}}', '{${ids.what.join( ',' )}}', '{${ids.why.join( ',' )}}')`
+	);
+};
+
+linkLoader.getLinks()
+	.then( ( links )=> {
+		let linkIds = {};
+
+		for( let type in links ) {
+			let ids = [];
+
+			if( links[type].length !== 0 ) {
+				for( let link of links[type] ) {
+					insertLinkIntoDatabase( link );
+					ids.push( link.id );
+				}
+			}
+
+			linkIds[ type ] = ids;
+		}
+
+		writeSummaryToDatabase( linkIds ).catch((err)=> {
+			console.log( err );
+			db.end();
+			process.exit(1);
+		}).then(()=>{
+			db.end();
+			process.exit(0);
+		});
+	});
